af034d23af1480d5af823b921d62d13ab206705a
Nodirbek Kadamov <kadamovnk@icloud.com>
2024-01-27 11:59:16 +0500

    Initial Commit

--- MapApp/Views/LocationDetailView.swift
+++ MapApp/Views/LocationDetailView.swift
@@ -6,13 +6,108 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationDetailView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    let location: Location
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                imageSection
+                    .shadow(color: .black.opacity(0.3), radius: 20, x: 0.0, y: 10)
+                
+                VStack(alignment: .leading, spacing: 16.0) {
+                    titleSection
+                    Divider()
+                    descriptionSection
+                    Divider()
+                    mapLayer
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+        .background(.ultraThinMaterial)
+        .overlay(backButton, alignment: .topLeading)
     }
 }
 
 #Preview {
-    LocationDetailView()
+    LocationDetailView(location: LocationsDataService.locations.first!)
+        .environmentObject(LocationsViewModel())
+}
+
+extension LocationDetailView {
+    private var imageSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIDevice.current.userInterfaceIdiom == .pad ? nil : UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(PageTabViewStyle())
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Text(location.name)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Text(location.cityName)
+                .font(.title3)
+                .foregroundColor(.secondary)
+        }
+    }
+    
+    private var descriptionSection: some View {
+        VStack(alignment: .leading, spacing: 16.0) {
+            Text(location.description)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            if let url = URL(string: location.link) {
+                Link("Read more on Wikipedia", destination: url)
+                    .font(.headline)
+                    .tint(.blue)
+            }
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: .constant(MKCoordinateRegion(
+            center: location.coordinates,
+            span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                   longitudeDelta: 0.01))),
+            annotationItems: [location]) { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+            }
+        }
+            .allowsHitTesting(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
+            .aspectRatio(1, contentMode: .fit)
+            .cornerRadius(30)
+    }
+    
+    private var backButton: some View {
+        Button {
+            vm.sheetLocation = nil
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+                .padding(16)
+                .foregroundColor(.primary)
+                .background(.thickMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 4)
+                .padding()
+        }
+    }
 }
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -6,13 +6,98 @@
 //
 
 import SwiftUI
+import MapKit
+
 
 struct LocationsView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            mapLayer.ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                    .frame(maxWidth: maxWidthForIpad)
+                Spacer()
+                locationsPreviewStack
+            }
+        }
+        .sheet(item: $vm.sheetLocation, onDismiss: nil) { location in
+            LocationDetailView(location: location)
+        }
     }
 }
 
 #Preview {
     LocationsView()
+        .environmentObject(LocationsViewModel())
+}
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Button(action: {
+                vm.toggleLocationsList()
+            }, label: {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation.name)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
+                    }
+            })
+            
+            
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: {location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) {
+                if vm.mapLocation == $0 {
+                    LocationPreviewView(location: $0)
+                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 20)
+                        .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading)))
+                }
+            }
+        }
+    }
 }
